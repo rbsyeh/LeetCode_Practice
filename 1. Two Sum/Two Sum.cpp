eef203c6608d3ae388af1f399778e400f824d9e9
Wen-Ching Yeh <kp0310j@gmail.com>
2021-02-02 12:54:13 +0800

    Create Two Sum.cpp

---
+++ 1. Two Sum/Two Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> ans(2);
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=0; j!=i; j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    if(i<j)
+                    {
+                        ans[0]=i;
+                        ans[1]=j;
+                    }
+                    else
+                    {
+                        ans[0]=j;
+                        ans[1]=i;
+                    }
+                    return ans;
+                }
+            }
+            
+        }
+        
+    }
+};
