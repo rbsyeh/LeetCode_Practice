8fad315d89030a038b3684c7032b770f45c725be
Wen-Ching Yeh <kp0310j@gmail.com>
2021-02-06 17:04:40 +0800

    Create Hamming Distance.cpp

---
+++ 461. Hamming Distance/Hamming Distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans=0;
+        for(int i=0; i<32; i++)\
+        {
+            if(x%2!=y%2)
+                ans++;
+            x=x/2, y=y/2;
+        }
+        return ans;
+        
+    }
+};
